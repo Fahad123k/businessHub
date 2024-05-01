0d4745c12e9a32e8d2e0f7df38b11aa0fd925878
Fahad123k <ahmad.nishu08@gmail.com>
2024-05-01 02:26:59 -0400

    add brand filter

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -5,21 +5,62 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Brand;
 class ShopController extends Controller
 {
     //index page of shop
     public function index(Request $request){
         $page=$request->query('page');
         $size=$request->query('size');
+        $order=$request->query('order');
         if(!$page)
             $page=1;
         if(!$size)
             $size=12;
+        if(!$order)
+            $order=-1;
+        $o_column="";
+        $o_order="";
+
+        switch ($order) {
+            case 1:
+                $o_column="created_at";
+                $o_order="DESC";
+                break;
+            case 2:
+                $o_column="created_at";
+                $o_order="ASC";
+                break;
+            case 3:
+                $o_column="regular_price";
+                $o_order="ASC";
+                break;
+            case 4:
+                $o_column="regular_price";
+                $o_order="DESC";
+                break;
+            
+            default:
+                $o_column="id";
+                $o_order="DESC";
+                break;
+        }
+
+        $brands= Brand::orderBy('name','ASC')->get();
+        // check box filter for brand
+        $q_brands=$request->query('brands');
 
 
-        $products=Product::paginate($size);
+        $products=Product::where(function($query) use($q_brands){
+            // if q brands is exist then where clause other wise q_brands will be empty like q_brand=
+            $query->whereIn('brand_id',explode(',',$q_brands))->orwhereRaw("'".$q_brands."'=''");
+        } )
+        ->orderBy('created_at','DESC')->orderBy($o_column, $o_order)->paginate($size);
       
-        return view('shop',['products'=>$products,'page'=>$page,'size'=>$size]);
+
+        return view('shop',
+        ['products'=>$products,'page'=>$page,'size'=>$size,'order'=>$order,'brands'=>$brands,'q_brands'=>$q_brands]);
+
         }
 
     public function productDetails($slug){
